49114cabdf591d8f419fce6b07f416ffd3c6172d
aaaaa <aaaaa>
2022-01-10 13:29:47 +0900

    フォアグランドサービスを実装。

--- app/src/main/java/com/tks/foregroundserviceandlocation/MainActivity.java
+++ app/src/main/java/com/tks/foregroundserviceandlocation/MainActivity.java
@@ -1,23 +1,37 @@
 package com.tks.foregroundserviceandlocation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.tks.foregroundserviceandlocation.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
-
-	private TextView mTextView;
-	private ActivityMainBinding binding;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-		binding = ActivityMainBinding.inflate(getLayoutInflater());
-		setContentView(binding.getRoot());
+		TLog.d("aaaaa");
 
-		mTextView = binding.text;
+		findViewById(R.id.btnStartService).setOnClickListener(view -> {
+			Intent intent = new Intent(MainActivity.this, FlcService.class);
+			intent.setAction(Constants.ACTION.START);
+			startForegroundService(intent);
+		});
+
+		findViewById(R.id.btnStopService).setOnClickListener(view -> {
+			Intent intent = new Intent(MainActivity.this, FlcService.class);
+			intent.setAction(Constants.ACTION.STOP);
+			startService(intent);
+		});
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		TLog.d("aaaaa");
 	}
 }
